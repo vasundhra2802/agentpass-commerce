77daf00e8296d89d12dc6b9004029e9dca9fb8b9
vasundhra2802 <vasundhra2802@gmail.com>
2026-08-30 19:40:12 +0530

    Allow local Vite ports in CORS

--- backend/main.py
+++ backend/main.py
@@ -99,7 +99,7 @@ payment_sessions = {}
 verified_payment_ids = set()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origin_regex=r"^http://(localhost|127\.0\.0\.1):\d+$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
